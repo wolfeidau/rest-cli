b9670f4a4a784e5581b8b0660b8ebad9939729f5
Mark Wolfe <mark@wolfe.id.au>
2022-05-27 13:28:05 +1000

    feat(cli): add a flag to enable dumping of request and response
    This should make it easier to debug things when troubleshooting.

---
+++ pkg/dump/dump_test.go
@@ -0,0 +1,38 @@
+package dump
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDumpTransport(t *testing.T) {
+
+	assert := require.New(t)
+
+	buf := &bytes.Buffer{}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+
+	server.Client().Transport = WrapTransport(buf, server.Client().Transport)
+
+	req := request("GET", server.URL, bytes.NewBufferString("{}"))
+
+	res, err := server.Client().Do(req)
+	assert.NoError(err)
+	assert.Equal(200, res.StatusCode)
+
+	assert.Contains(buf.String(), "{ }\n")
+}
+
+func request(method, url string, body io.Reader) *http.Request {
+	req, _ := http.NewRequest(method, url, body)
+
+	return req
+}
